9eebac4f32630a7227afa71ae65e50192a491b75
Artem Zuev <info@holyzlo.ru>
2022-09-05 13:15:13 +0300

    Пройдено Spring Data JPA(3.3)

--- src/main/java/tacos/TacoOrder.java
+++ src/main/java/tacos/TacoOrder.java
@@ -1,10 +1,10 @@
 package tacos;
 
 import lombok.Data;
+import lombok.Generated;
 import org.hibernate.validator.constraints.CreditCardNumber;
-import org.springframework.data.annotation.Id;
-import org.springframework.data.relational.core.mapping.Table;
 
+import javax.persistence.*;
 import javax.validation.constraints.Digits;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Pattern;
@@ -14,11 +14,13 @@ import java.util.Date;
 import java.util.List;
 
 @Data
-@Table
+@Entity
 public class TacoOrder implements Serializable {
     private static final long serialVersionUID = 1L;
     @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
+
     private Date placedAt;
 
     @NotBlank(message="Delivery name is required")
@@ -38,6 +40,7 @@ public class TacoOrder implements Serializable {
     private String ccExpiration;
     @Digits(integer=3, fraction=0, message="Invalid CVV")
     private String ccCVV;
+    @OneToMany(cascade = CascadeType.ALL)
     private List<Taco> tacos = new ArrayList<>();
 
     public void addTaco(Taco taco) {
--- src/main/java/tacos/data/OrderRepository.java
+++ src/main/java/tacos/data/OrderRepository.java
@@ -1,7 +1,16 @@
 package tacos.data;
 
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import tacos.TacoOrder;
 
+import java.util.List;
+
 public interface OrderRepository extends CrudRepository<TacoOrder, Long> {
+    @Query("Order o where o.deliveryCity='Seattle'")
+    List<TacoOrder> readOrdersDeliveredInSeattle();
+    List<TacoOrder> findByDeliveryCityOrderByDeliveryTo(String city);
+
+    List<TacoOrder> findByDeliveryToAndDeliveryCityAllIgnoresCase(
+            String deliveryTo, String deliveryCity);
 }
