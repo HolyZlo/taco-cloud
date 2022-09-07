970cec14a6eaf29b6c6044fd381f3463dc78faa3
Artem Zuev <info@holyzlo.ru>
2022-09-07 13:40:29 +0300

    Пройдена Глава 6 Конфигурация

--- src/main/java/tacos/TacoCloudApplication.java
+++ src/main/java/tacos/TacoCloudApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Profile;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import tacos.data.IngredientRepository;
 import tacos.Ingredient.Type;
@@ -22,8 +23,9 @@ public class TacoCloudApplication {
         SpringApplication.run(TacoCloudApplication.class, args);
     }
 
+    @Profile("!prod")
     @Bean
-    public ApplicationRunner dataLoader(IngredientRepository repo, UserRepository userRepo) {
+    public ApplicationRunner dataLoaderDev(IngredientRepository repo, UserRepository userRepo) {
         return args -> {
             repo.save(new Ingredient("FLTO", "Flour Tortilla", Type.WRAP));
             repo.save(new Ingredient("COTO", "Corn Tortilla", Type.WRAP));
@@ -39,6 +41,22 @@ public class TacoCloudApplication {
         };
     }
 
+    @Profile("prod")
+    @Bean
+    public ApplicationRunner dataLoaderProd(IngredientRepository repo, UserRepository userRepo) {
+        return args -> {
+            repo.save(new Ingredient("FLTO", "Flour Tortilla", Type.WRAP));
+            repo.save(new Ingredient("COTO", "Corn Tortilla", Type.WRAP));
+            repo.save(new Ingredient("GRBF", "Ground Beef", Type.PROTEIN));
+            repo.save(new Ingredient("CARN", "Carnitas", Type.PROTEIN));
+            repo.save(new Ingredient("TMTO", "Diced Tomatoes", Type.VEGGIES));
+            repo.save(new Ingredient("LETC", "Lettuce", Type.VEGGIES));
+            repo.save(new Ingredient("CHED", "Cheddar", Type.CHEESE));
+            repo.save(new Ingredient("JACK", "Monterrey Jack", Type.CHEESE));
+            repo.save(new Ingredient("SLSA", "Salsa", Type.SAUCE));
+            repo.save(new Ingredient("SRCR", "Sour Cream", Type.SAUCE));
+        };
+    }
 
 
 }
